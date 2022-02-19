bedea297f9c3cbd4993afa668bb26be9fb346642
soyeolesafm <soyelesafm@hotmail.com>
2022-02-19 14:42:29 -0600

    playing with inner.HTML and buttons

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const btn = document.querySelector(`.btn`)
 const phonyBtn = document.querySelector(`.phony_btn`)
-
+const topMessage =document.querySelector('#top_message').innerHTML  
 btn.addEventListener(`click`, (e) => {
-    alert('click')
+    document.getElementById('top_message').innerHTML = ""
+    document.getElementById('bottom_message').innerHTML = "I just did"
 } )
 
 phonyBtn.addEventListener(`click`, (e) => {
-    alert ('porque me touchas?')
+    document.getElementById('top_message').innerHTML = "How rude!"
 })
+
