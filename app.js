371aff59075d5a25744ec53b65264a8924a46323
soyeolesafm <soyelesafm@hotmail.com>
2022-02-19 09:09:22 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(`.btn`)
+const phonyBtn = document.querySelector(`.phony_btn`)
+
+btn.addEventListener(`click`, (e) => {
+    alert('click')
+} )
+
+phonyBtn.addEventListener(`click`, (e) => {
+    alert ('porque me touchas?')
+})
