2c60705cd92899adacf5a37e5c0554f4477944b1
soyeolesafm <soyelesafm@hotmail.com>
2022-02-20 10:29:57 -0600

    playing with buttons

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const btn = document.querySelector(`.btn`)
 const phonyBtn = document.querySelector(`.phony_btn`)
-const topMessage =document.querySelector('#top_message').innerHTML  
+let phonyBtnActive = false
 btn.addEventListener(`click`, (e) => {
-    document.getElementById('top_message').innerHTML = ""
-    document.getElementById('bottom_message').innerHTML = "I just did"
+    if (phonyBtnActive) { return
+        
+    } else {
+        document.getElementById('top_message').innerHTML = ""
+        document.getElementById('bottom_message').innerHTML = "I just did"
+        phonyBtn.style.display = "none"
+        btn.style.display = "none"
+    }
+
+    
 } )
 
 phonyBtn.addEventListener(`click`, (e) => {
     document.getElementById('top_message').innerHTML = "How rude!"
+    phonyBtn.style.display = "none"
+    phonyBtnActive = true
 })
 
